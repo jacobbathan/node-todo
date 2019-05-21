cd01f7e34e21f2ccfb638bc4935c6e7f9afc227e
jacob <jacobbathan@gmail.com>
2019-05-21 13:07:18 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes.js");
+
+// const notes = getNotes();
+
+// console.log(notes);
+// console.log(chalk.green("Success!"));
+// console.log(chalk.red.inverse.bold("Error!"));
+// console.log(process.argv[2]);
+
+console.log(process.argv);
+console.log(yargs.argv);

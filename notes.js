4176a81e0b979e24a6e121fb5d61551f5878aaa6
jacobbathan <jacobbathan@gmail.com>
2019-05-21 20:42:24 -0700

    add note functionality working

--- notes.js
+++ notes.js
@@ -6,17 +6,26 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
   const notes = loadNotes();
-  notes.push({
-    title: title,
-    body: body,
+  const duplicateNotes = notes.filter(note => {
+    return note.title === title;
   });
-    saveNotes(notes)
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+      saveNotes(notes);
+      console.log('new note added')
+  } else {
+      console.log('Note title exists')
+  }
 };
 
 const saveNotes = notes => {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
-}
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
 
 const loadNotes = () => {
   try {

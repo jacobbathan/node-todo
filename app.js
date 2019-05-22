1f663d4d9376ba2658e661f3e815680db127963e
jacobbathan <jacobbathan@gmail.com>
2019-05-21 20:23:01 -0700

    added yargs, configuring app.js

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
-const chalk = require("chalk");
-const yargs = require("yargs");
-const getNotes = require("./notes.js");
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
 // const notes = getNotes();
 
-// console.log(notes);
-// console.log(chalk.green("Success!"));
-// console.log(chalk.red.inverse.bold("Error!"));
-// console.log(process.argv[2]);
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(process.argv);
-console.log(yargs.argv);
+// create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: argv => {
+    notes.addNote(argv.title, argv.body)
+  },
+});
+
+// create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'remove a note',
+  handler: () => {
+    console.log('removing note');
+  },
+});
+
+// create list command
+yargs.command({
+  command: 'list',
+  describe: 'list a note',
+  handler: () => {
+    console.log('listing notes');
+  },
+});
+
+// create read command
+yargs.command({
+  command: 'read',
+  describe: 'read note',
+  handler: () => {
+    console.log('reading notes');
+  },
+});
+
+yargs.parse();
+//console.log(yargs.argv);
--- notes.js
+++ notes.js
@@ -1,5 +1,34 @@
+const fs = require('fs');
+
 const getNotes = () => {
-    return "Your notes..."
+  return 'Your notes...';
+};
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  notes.push({
+    title: title,
+    body: body,
+  });
+    saveNotes(notes)
+};
+
+const saveNotes = notes => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
 }
 
-module.exports = getNotes
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
